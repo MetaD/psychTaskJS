1f43a3a201013dddb849f20722514a07509fb295
Meng Du <mengdu@umich.edu>
2016-06-20 18:22:16 -0400

    switching to jspsych

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# psychTaskJS
-Set up psychology experiments in web pages and get participants all over the Internet.
+# sharingTaskJS
+
---
+++ sharing-task.js
@@ -0,0 +1,8 @@
+var hello_trial = {
+    type: 'text',
+    text: 'Hello world!'
+}
+
+jsPsych.init({
+    timeline: [ hello_trial ]
+})
